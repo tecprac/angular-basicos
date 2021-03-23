fbc6f81334c0e1b7253ffe8f018da36a9d61f84f
tecprac <amontalvo@tecnologiapractica.com.mx>
2021-03-23 08:55:41 -0600

    primer commint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent  {
+
+    heroeBorrado : string = '';
+
+    heroes: string[] = ['Spiderman','Ironman','Hulk','Thor'];
+
+
+    borrarHeroe(){
+        this.heroeBorrado = this.heroes.shift() || '';
+    }
+
+}
